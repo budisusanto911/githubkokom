7efd1720ec799f20a77b1e37428bf0acd7e00b48
Handi Komara <handikomara22@gmail.com>
2021-12-20 16:44:10 +0700

    add function, navbar & home

--- app/src/main/java/com/udacoding/wisatamvvmarcsimple/ui/home/Home.kt
+++ app/src/main/java/com/udacoding/wisatamvvmarcsimple/ui/home/Home.kt
@@ -1,23 +1,34 @@
 package com.udacoding.wisatamvvmarcsimple.ui.home
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.view.Menu
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MenuItem
+import androidx.navigation.ui.*
 import com.udacoding.wisatamvvmarcsimple.R
 import com.udacoding.wisatamvvmarcsimple.databinding.ActivityHomeBinding
+import com.udacoding.wisatamvvmarcsimple.ui.home.ui.gallery.GalleryFragment
+import com.udacoding.wisatamvvmarcsimple.ui.home.ui.home.HomeFragment
+import com.udacoding.wisatamvvmarcsimple.ui.home.ui.slideshow.SlideshowFragment
+import androidx.navigation.ui.NavigationUI
 
-class Home : AppCompatActivity() {
+
+
+
+class Home : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener, NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var  toggle : ActionBarDrawerToggle
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,33 +37,48 @@ class Home : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.appBarHome.toolbar)
+       /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+        */
 
-        binding.appBarHome.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_home)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
+        toggle = ActionBarDrawerToggle(
+            this, binding.drawerLayout, binding.appBarHome.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.home, menu)
-        return true
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(this)
+        binding.btnMain.setOnNavigationItemSelectedListener(this)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_home)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment_content_home, fragment)
+                .commit()
+            return true
+        }
+        return false
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        for (fragment in supportFragmentManager.fragments) {
+            supportFragmentManager.beginTransaction().remove(fragment!!).commit()
+        }
+        var fragment: Fragment? = null
+        when (item.itemId) {
+            R.id.home_menu -> fragment = HomeFragment()
+            R.id.search_menu -> fragment = GalleryFragment()
+            R.id.favorite_menu -> fragment = SlideshowFragment()
+            R.id.account_menu -> {
+                binding.drawerLayout.openDrawer(GravityCompat.START)
+                return false
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START);
+        return loadFragment(fragment)
     }
 }
--- app/src/main/java/com/udacoding/wisatamvvmarcsimple/ui/login/Login.kt
+++ app/src/main/java/com/udacoding/wisatamvvmarcsimple/ui/login/Login.kt
@@ -1,12 +1,30 @@
 package com.udacoding.wisatamvvmarcsimple.ui.login
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.udacoding.wisatamvvmarcsimple.R
+import com.udacoding.wisatamvvmarcsimple.databinding.ActivityLoginBinding
+import com.udacoding.wisatamvvmarcsimple.ui.forgot.ForgotPasswordActivity
+import com.udacoding.wisatamvvmarcsimple.ui.home.Home
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.run {
+            forgot.setOnClickListener {
+                val intent = Intent(this@Login, ForgotPasswordActivity::class.java)
+                startActivity(intent)
+            }
+            btnLogin.setOnClickListener {
+                val intent = Intent(this@Login, Home::class.java)
+                startActivity(intent)
+            }
+        }
+
     }
 }
